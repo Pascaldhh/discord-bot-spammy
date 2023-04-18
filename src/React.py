6572edaf50df689710861db48c6108700bbdbcbb
Pascaldhh <haan.pascal@hotmail.com>
2023-04-18 15:45:02 +0200

    Bot can react to messages

---
+++ src/React.py
@@ -0,0 +1,14 @@
+from random import randint
+from discord.ext import commands
+
+class React(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+    self.emojis = ["😀", "😃", "😄", "😁", "😆", "🥹", "😅", "😂", "🤣", "🥲", "☺️", "😊", "😇", "🙂", "🙃", "😉", "😌", "😍", "🥰", "😘"]
+
+  @commands.hybrid_command(name="react", description="Bot reacts with random emoji")
+  async def reactEmoji(self, ctx):
+    await ctx.message.add_reaction(self.emojis[randint(0, len(self.emojis)-1)])
+
+async def setup(bot):
+  await bot.add_cog(React(bot))
